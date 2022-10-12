18a803cd815bdd5bddb7c1c97f2750fcceb59782
mazharul <mazharul@redlimesolutions.com>
2022-10-12 20:33:43 +0600

    const explored in details & got to know the pros and cons

---
+++ 2. Const/const.js
@@ -0,0 +1,12 @@
+// variables value can't be re assigned variable declared with const keyword
+// must be assigned a value when declaring a variable with const keyword
+// variable declared with const keyword is block scope like let keyword.
+
+// Suggested to use const during declaration of new Array, New Object, New Function, New RegEx
+
+const PI = 3.1416;
+console.log(PI);
+
+// note: for Array , Object declared with const keyword property can be modified but can't changed whole element.
+
+
--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 -learned about block scoping of var-let  and hoisting differences between let and var
 
 ## const:
-
+-const explored in details & got to know the pros and cons
 
