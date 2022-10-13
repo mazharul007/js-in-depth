d87dd149a611ce6f9f469c388607854fa7b9f66c
mazharul <mazharul@redlimesolutions.com>
2022-10-13 21:31:35 +0600

    Explored data type

---
+++ 4. Data Types/data.js
@@ -0,0 +1,16 @@
+ // js evaluates expression from left to right 
+
+ console.log(15+4+"test string"); // output: 19test string
+ console.log("string" + 15+4); // output: string154
+
+ // Data Types:
+ /**
+  * String // ""
+  * Numbers // 2,3,4
+  * Booleans // true, false
+  * Arrays // []
+  * Objects // {}
+  * Undefined // its a value Undefined is treated as a value in JS // Note: empty string is not undefined
+  **/ 
+
+
