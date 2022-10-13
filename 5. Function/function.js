7719c53bb640228fc73630206bb4a0859df1c5d4
mazharul <mazharul@redlimesolutions.com>
2022-10-13 22:57:34 +0600

    explored function

---
+++ 5. Function/function.js
@@ -0,0 +1,17 @@
+// function in action 
+
+
+function greet(name){
+    return "Hello greetings, " + name;
+}
+let greetings = greet(" Mazharul Hoque Naumy");
+console.log(greetings);
+
+// notes: Here name is parameter and Mazharul Hoque Naumy is an argument which is value and passed inside greet function call
+// by default function implicitly return a undefined value if a function doesn't return any value
+
+
+
+
+
+ 
