aa3a54792265a76a0c04aa99cdf4ae6d1964d8c0
mazharul <mohammad.mazharul.hoque.007@gmail.com>
2022-11-03 21:38:38 +0600

    Numbers in JS

---
+++ 10. Numbers/numers.js
@@ -0,0 +1,21 @@
+// string / 10 will evaluate NaN 
+
+let val = "1b0";
+
+console.log(isNaN(val));
+
+// interview question : type of NaN is number
+console.log(typeof(NaN));
+
+ 
+// Number methods
+
+let num = 10;
+console.log(typeof(num.toString()));
+
+
+let str = "123";
+console.log(typeof Number(str));
+
+
+
